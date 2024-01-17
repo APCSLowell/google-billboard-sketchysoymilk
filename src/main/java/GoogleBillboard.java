5ee72a99f4c8a3f48d97a378340b0ec7dcfe29e7
mrchanr <42151767+mrchanr@users.noreply.github.com>
2024-01-17 12:06:39 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,10 +1,17 @@
 public class GoogleBillboard {
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
+		    //your code here 
+    //most of our coding here
+    // for loop
+        // check if the number is prime
+        System.out.println(7182818284);
 	}
 
 
 	//Finish this function
-	public boolean isPrime(){
+	public boolean isPrime(double dNum){
+		//your code here 
+    		//copy and pasted from coding
+    		return true;  
 	}
