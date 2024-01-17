9cfc5fb0a067ecb8cb2cc2b9a6bda40a62d2338a
sketchysoymilk <143027784+sketchysoymilk@users.noreply.github.com>
2024-01-17 11:45:36 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,17 +1,30 @@
 public class GoogleBillboard {
-// feel free to delete this comment
-//Your old code might've worked, but it didnt because isPrime() needs to be a static function in order to call it inside main() function
-//since main() is also static.
-
-	
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
-		System.out.println("Not the right string, tests will fail!");
-	}
-
-
-	//Finish this function
-	public boolean isPrime(){
-	}
+		public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+{   
+  for(int i = 2,  m = 12; (m < e.length()-10 || i < e.length()-10); i++, m++){
+    String digits = e.substring(i,m);
+    double dNum = Double.parseDouble(digits);
+    if(isPrime(dNum) == true){
+      System.out.println(dNum);
+      break;
+    }
+  }
+}  
+public void draw()  
+{   
+  //not needed for this assignment
+}  
+public static boolean isPrime(final double dNum)  
+{   
+  if(dNum < 2)
+    return false;
+  for(int i = 2; i <= Math.sqrt(dNum); i++){
+    if(dNum%i == 0)
+      return false;
+  } 
+  return true;  
+} 
 
 }
