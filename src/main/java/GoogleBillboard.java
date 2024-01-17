f12bc9d10ab6ee40e9c4399b984125a6620b89c3
sketchysoymilk <143027784+sketchysoymilk@users.noreply.github.com>
2024-01-17 12:14:51 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,18 +1,24 @@
-public class GoogleBillboard {
-	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
-	public static void main(final String[] args) {
-		    //your code here 
-    //most of our coding here
-    // for loop
-        // check if the number is prime
-        System.out.println("7.427466391E9\n");
-	}
-
-
-	//Finish this function
-	public static boolean isPrime(double dNum){
-		//your code here 
-    		//copy and pasted from coding
-    		return true;  
-	}
-}// end of GoogleBillboard class
+public class GoogleBillboard{
+public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
+public static void main(final String[] args)  
+{   
+  for(int i = 2,  m = 12; (m < e.length()-10 || i < e.length()-10); i++, m++){
+    String digits = e.substring(i,m);
+    double dNum = Double.parseDouble(digits);
+    if(isPrime(dNum) == true){
+      System.out.println(dNum);
+      break;
+    }
+  }
+}  
+public static boolean isPrime(double dNum)  
+{   
+  if(dNum < 2)
+    return false;
+  for(int i = 2; i <= Math.sqrt(dNum); i++){
+    if(dNum%i == 0)
+      return false;
+  } 
+  return true;  
+} 
+}
