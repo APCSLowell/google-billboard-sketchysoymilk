ec0d5aee87efe147f871f8fe31ca8f5078f4a581
StephenMiner <64863697+StephenMiner@users.noreply.github.com>
2024-01-16 17:43:57 -0800

    Update GoogleBillboard.java

--- src/main/java/GoogleBillboard.java
+++ src/main/java/GoogleBillboard.java
@@ -1,4 +1,9 @@
 public class GoogleBillboard {
+// feel free to delete this comment
+//Your old code might've worked, but it didnt because isPrime() needs to be a static function in order to call it inside main() function
+//since main() is also static.
+
+	
 	public final static String e = "2.7182818284590452353602874713526624977572470936999595749669676277240766303535475945713821785251664274274663919320030599218174135966290435";  
 	public static void main(final String[] args) {
 		System.out.println("Not the right string, tests will fail!");
